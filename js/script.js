250d98c0da32009fa87c8507edc545c31b020794
sarathkumar padmanaban <sarathkumar.padmanaban@gmail.com>
2024-02-21 18:35:20 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+	// Sticky Navbar
+    let header = document.querySelector('header');
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+     
+    // Dark Mode
+    let darkmode = document.querySelector('#darkmode');
+     
+    darkmode.onclick = () => {
+        if(darkmode.classList.contains('bx-moon')){
+            darkmode.classList.replace('bx-moon','bx-sun');
+            document.body.classList.add('active');
+        }else{
+            darkmode.classList.replace('bx-sun','bx-moon');
+            document.body.classList.remove('active');
+        }
+    }
+
+    function sendEmail() {
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var message = document.getElementById('message').value;
+
+        // You can replace 'your@email.com' with your actual email address
+        var mailtoLink = 'mailto:sarathkumar.padmanaban@gmail.com' +
+            '?subject=' + encodeURIComponent('Message from ' + name) +
+            '&body=' + encodeURIComponent(message + '\n\nSender Email: ' + email);
+
+        window.location.href = mailtoLink;
+        location.reload();
+    }
